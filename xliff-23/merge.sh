f68bc789b92723e907bc620e520b472bc0c80b2a
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-06-16 11:25:09 -0300

    Initial provenance draft

--- xliff-23/merge.sh
+++ xliff-23/merge.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home
 XML_FILE=xliff-extended-v2.3-part2.xml
 rm -f $XML_FILE
-java -cp lib/merger.jar com.maxprograms.xml.Merger xliff23.xml $XML_FILE
+$JAVA_HOME/bin/java -cp lib/merger.jar com.maxprograms.xml.Merger xliff23.xml $XML_FILE
